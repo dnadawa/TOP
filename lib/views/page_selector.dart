ea4dc3176264bf5f430820f8c4ec2ad2b4b2e3ce
dnadawa <dulajnadawa@gmail.com>
2022-08-05 13:44:52 +0530

    Add bottom navigation bar

---
+++ lib/views/page_selector.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:top/constants.dart';
+
+class PageSelector extends StatefulWidget {
+
+  @override
+  State<PageSelector> createState() => _PageSelectorState();
+}
+
+class _PageSelectorState extends State<PageSelector> {
+  final PageController controller = PageController();
+  int currentIndex = 0;
+
+  @override
+  void initState(){
+    super.initState();
+    if(mounted){
+      controller.addListener(() {
+        setState(() => currentIndex = controller.page!.toInt());
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: controller,
+        physics: NeverScrollableScrollPhysics(),
+        children: [
+          Container(width: double.infinity, height: double.infinity,color: Colors.red,),
+          Container(width: double.infinity, height: double.infinity,color: Colors.green,),
+          Container(width: double.infinity, height: double.infinity,color: Colors.blue,),
+          Container(width: double.infinity, height: double.infinity,color: Colors.amber,),
+          Container(width: double.infinity, height: double.infinity,color: Colors.purple,),
+        ],
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: kGreen,
+        unselectedItemColor: kDisabled,
+        showUnselectedLabels: true,
+        selectedLabelStyle: TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w400),
+        unselectedLabelStyle: TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w400),
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        onTap: (index){
+          controller.animateToPage(index, duration: Duration(milliseconds: 100), curve: Curves.easeInOut);
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.event_available), label: 'Availability'),
+          BottomNavigationBarItem(icon: Icon(Icons.assignment), label: 'My Shifts'),
+          BottomNavigationBarItem(icon: Icon(Icons.work), label: 'Released'),
+          BottomNavigationBarItem(icon: Icon(Icons.pending_actions), label: 'Time Sheet'),
+        ],
+      ),
+    );
+  }
+}
