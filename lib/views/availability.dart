fc0d9cbfa0f36f7347bb5a1fa0e1d8fd223642eb
dnadawa <dulajnadawa@gmail.com>
2022-08-09 19:53:47 +0530

    Add key to availability

--- lib/views/availability.dart
+++ lib/views/availability.dart
@@ -7,6 +7,7 @@ import 'package:top/widgets/availability_tile.dart';
 import 'package:top/widgets/backdrop.dart';
 import 'package:top/widgets/button.dart';
 import 'package:top/widgets/heading_card.dart';
+import 'package:top/widgets/badge.dart';
 
 class Availability extends StatelessWidget {
   @override
@@ -17,24 +18,65 @@ class Availability extends StatelessWidget {
           padding: EdgeInsets.all(30.w),
           child: Column(
             children: [
-              SizedBox(height: ScreenUtil().statusBarHeight),
+              SizedBox(height: 30.h),
 
               Expanded(
                 child: HeadingCard(
                   title: 'Availability',
                   child: Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 15.w),
-                      child: ListView.builder(
-                        physics: BouncingScrollPhysics(),
-                        itemCount: 10,
-                        itemBuilder: (context, i) => AvailabilityTile(
-                          dateString: 'Wed Aug 2',
-                          AM: true,
-                          PM: true,
-                          NS: true,
+                    child: Column(
+                      children: [
+                        SizedBox(height: ScreenUtil().statusBarHeight),
+
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 10.w),
+                              child: SizedBox(
+                                  width: 25.w,
+                                  height: 20.h,
+                                  child: Badge(text: '', color: Colors.green, enabled: true),
+                              ),
+                            ),
+                            Text('Available', style: TextStyle(fontSize: 14.sp)),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 10.w),
+                              child: SizedBox(
+                                  width: 25.w,
+                                  height: 20.h,
+                                  child: Badge(text: '', color: Colors.red, enabled: true),
+                              ),
+                            ),
+                            Text('Booked', style: TextStyle(fontSize: 14.sp)),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 10.w),
+                              child: SizedBox(
+                                  width: 25.w,
+                                  height: 20.h,
+                                  child: Badge(text: '', color: Colors.green, enabled: false),
+                              ),
+                            ),
+                            Text('Not Available', style: TextStyle(fontSize: 14.sp)),
+                          ],
                         ),
-                      ),
+
+                        Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 15.w),
+                            child: ListView.builder(
+                              physics: BouncingScrollPhysics(),
+                              itemCount: 10,
+                              itemBuilder: (context, i) => AvailabilityTile(
+                                dateString: 'Wed Aug 2',
+                                AM: true,
+                                PM: true,
+                                NS: true,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
